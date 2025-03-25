592017189b0e04b3afa20c19b84ec3abe4cff64b
ycrangelo <yocorangelo18@gmail.com>
2025-03-25 09:02:43 +0800

    deactive active account type

--- accountType.py
+++ accountType.py
@@ -60,3 +60,60 @@ def register_accountType_routes(app):
         except Exception as e:
             print(f"Error: {e}")
             return jsonify({"error": str(e)}), 500
+        
+        
+    @app.route('/accountType/deactivation', methods=['GET', 'POST'])
+    def accountTypeDeactivation():
+        # Connect to the database
+        mydb = get_db_connection()
+        my_cursor = mydb.cursor()
+
+        try:
+            # Process form data
+            data = request.get_json()
+            item_id = data['id']
+            codeId=data['codeId']
+            
+            my_cursor.execute("SELECT status FROM account_type WHERE id = %s", (item_id,))
+            logs = my_cursor.fetchone()  # Fetch all rows
+            user_status = logs[0]
+            print(logs)
+            if user_status =="1":
+                deactivation = 0
+                # Perform the update operation
+                my_cursor.execute("""
+                    UPDATE account_type
+                    SET status = %s WHERE id = %s
+                """, (deactivation, item_id))
+                mydb.commit()
+                # Log the update to the audit log
+                my_cursor.execute("INSERT INTO auditLogs (username, did) VALUES (%s, %s)", 
+                            (session['username'], f"Deactivate an account in Inventory with code ID: {codeId}"))
+            if user_status =="0":
+                deactivation = 1
+                # Perform the update operation
+                my_cursor.execute("""
+                    UPDATE account_type
+                    SET status = %s WHERE id = %s
+                """, (deactivation, item_id))
+                mydb.commit()
+            # Log the update to the audit log
+                my_cursor.execute("INSERT INTO auditLogs (username, did) VALUES (%s, %s)", 
+                            (session['username'], f"Activate an account in Inventory with code ID: {codeId}"))
+
+            # Commit changes to the database
+            mydb.commit()
+
+            # Return a success response
+            return jsonify({"message": "Inventory Deactivate successfully!"}), 200
+
+        except Exception as e:
+            # Capture the exact error for logging and return it in the response
+            mydb.rollback()
+            print(f"Error occurred: {e}")  # This will print to your console
+            return jsonify({"error": f"An error occurred while updating the inventory: {str(e)}"}), 500
+
+        finally:
+            # Close the database connection
+            my_cursor.close()
+            mydb.close()
